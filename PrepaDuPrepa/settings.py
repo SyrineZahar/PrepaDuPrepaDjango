ddfed4a0f800aa9bcbd57d8cb5dd3a5b9655ba43
Sofien Laghouanem <Sofienlaghouanem@gmail.com>
2024-05-22 22:01:18 +0100

    update navbar / setting / login script

--- PrepaDuPrepa/settings.py
+++ PrepaDuPrepa/settings.py
@@ -29,11 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'ProjectDjango/static')
-]
+
 
 # Application definition
 
@@ -125,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'PrepaDuPrepaDjango/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
